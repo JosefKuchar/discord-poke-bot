b72bb1a5cafb1650e240e6d2933f001cea4b95e7
Josef Kuchař <josef.kuchar267@gmail.com>
2021-03-05 13:21:45 +0100

    First working version

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ dotenv.config();
 
 const client = new Discord.Client();
 
+// Voice channel ids
+const id1 = '817359734893445170';
+const id2 = '817359978746347530';
+
+// Calls when connected to API
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  // Set activity for help
+  client.user.setActivity("!poke @Jméno", {
+    type: "LISTENING",
+  });
 });
 
 client.on('message', msg => {
@@ -18,9 +28,34 @@ client.on('message', msg => {
   const command = args.shift().toLowerCase();
 
   if (command === 'poke') {
-    const user = msg.mentions.users.first();
-    msg.reply(user.id);
+    const user = msg.mentions.members.first();
+
+    if (user) {
+      msg.reply('👉 ' + user.displayName);
+      if (args.length > 1) {
+        const count = parseInt(args[1]);
+        if (count > 0 && count <= 10) {
+          moveUser(count, user);
+        } else {
+          msg.reply('Dej tam normální počet týpku!');
+        }
+      } else {
+        moveUser(5, user);
+      }
+    }
   }
 });
+
+function moveUser(count, user) {
+  if (count === 0) {
+    return;
+  }
+
+  setTimeout(() => {
+    user.voice.setChannel(count % 2 == 0 ? id1 : id2)
+
+    moveUser(count - 1, user);
+  }, 1);
+}
 
 client.login(process.env.TOKEN);
